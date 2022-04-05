8b587d9bdb27031eefb7a7d37dfbe41280e4f4e7
iscaca <ismailcan9534@gmail.com>
2022-04-04 20:44:48 -0500

    ec2.tf is modified

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -2,7 +2,10 @@ resource "aws_instance" "my_ec2" {
   ami           = data.aws_ami.amazon_linux_2.image_id                           
   instance_type = var.instance_type                               #  "" String, Hard Coded Value
   tags = {
-    Name = var.env
+    Name = "${var.env}-app-instance"
   }
   vpc_security_group_ids = [aws_security_group.my_sg.id]
 }
+
+# Combine static and dynamic values together "${var.env}"
+# Name = dev-app-instance
