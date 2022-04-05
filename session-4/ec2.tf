50f794f8b17a035249fc0ed1714fe0c629643bfb
iscaca <ismailcan9534@gmail.com>
2022-04-04 21:01:23 -0500

    ec2.tf is modified

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "my_ec2" {
   instance_type = var.instance_type                               #  "" String, Hard Coded Value
   tags = {
     Name = "${var.env}-app-instance"
+    Name2 = format("%s-app-instance", var.env)
   }
   vpc_security_group_ids = [aws_security_group.my_sg.id]
 }
