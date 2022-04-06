976639d33cbc4fac12e537b75f03dbfa103058fd
iscaca <ismailcan9534@gmail.com>
2022-04-06 17:50:46 -0500

    vpc.tf is modified

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -16,3 +16,29 @@ variable "cidr_block"{
     default = [ "10.0.0.0/16", "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24" ]
 }
 
+
+variable "tagging_route_tables" {
+    type = list
+    description = "These are the names of Route Tables"
+    default = ["Public_Route_Table", "Private_Route_Table"]
+}
+
+variable "tagging_subnets_public" {
+    type = list
+    description = "These are the name of the each subnet under public"
+    default = [ "public-subnet-1a", "public-subnet-1b", "public-subnet-1c" ]
+  }
+
+  variable "tagging_subnets_private" {
+    type = list
+    description = "These are the name of the each subnet under public"
+    default = [ "private-subnet-1a", "private-subnet-1b", "private-subnet-1c" ]
+  }
+
+variable "availability_zones" {
+    type = list
+    description = "These are the list of availability zones in the Region"
+    default = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
+}
+
+
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -5,3 +5,75 @@ resource "aws_vpc" "my_own_vpc" {
         Name = var.tag
     }
 }
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_own_vpc.id
+  tags = {
+    Name = var.tagging_route_tables[0]
+  }
+}
+
+resource "aws_subnet" "Public_Subnet_1a" {
+  vpc_id     = aws_vpc.my_own_vpc.id
+  cidr_block = var.cidr_block[1]
+  availability_zone = var.availability_zones[0]
+  tags = {
+    Name = var.tagging_subnets_public[0]
+  }
+}
+
+resource "aws_subnet" "Public_Subnet_1b" {
+  vpc_id     = aws_vpc.my_own_vpc.id
+  cidr_block = var.cidr_block[2]
+  availability_zone = var.availability_zones[1]
+  tags = {
+    Name = var.tagging_subnets_public[1]
+  }
+}
+
+resource "aws_subnet" "Public_Subnet_1c" {
+  vpc_id     = aws_vpc.my_own_vpc.id
+  cidr_block = var.cidr_block[3]
+  availability_zone = var.availability_zones[2]
+  tags = {
+    Name = var.tagging_subnets_public[2]
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_own_vpc.id
+  tags = {
+    Name = var.tagging_route_tables[1]
+  }
+}
+
+resource "aws_subnet" "Private_Subnet_1a" {
+  vpc_id     = aws_vpc.my_own_vpc.id
+  cidr_block = var.cidr_block[4]
+  availability_zone = var.availability_zones[3]
+  tags = {
+    Name = var.tagging_subnets_private[0]
+  }
+}
+
+resource "aws_subnet" "Private_Subnet_1b" {
+  vpc_id     = aws_vpc.my_own_vpc.id
+  cidr_block = var.cidr_block[4]
+  availability_zone = var.availability_zones[4]
+  tags = {
+    Name = var.tagging_subnets_private[1]
+  }
+}
+
+resource "aws_subnet" "Private_Subnet_1c" {
+  vpc_id     = aws_vpc.my_own_vpc.id
+  cidr_block = var.cidr_block[4]
+  availability_zone = var.availability_zones[5]
+  tags = {
+    Name = var.tagging_subnets_private[2]
+  }
+}
+
+
+
+
