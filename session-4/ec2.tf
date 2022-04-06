bebd4a89f73f76e2fb3b65a27c2227da70a5b5aa
iscaca <ismailcan9534@gmail.com>
2022-04-06 15:26:58 -0500

    vpc folder and providers.tf is added

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "my_ec2" {
   ami           = data.aws_ami.amazon_linux_2.image_id                           
   instance_type = var.instance_type                               #  "" String, Hard Coded Value
   tags = {
-    Name = "${var.env}-app-instance"
-    Name2 = format("%s-app-instance", var.env)
+    Name = "${var.env}-app-instance"                              #Syntax may change
+    Name2 = format("%s-app-instance", var.env)                    #Syntax will never change
   }
   vpc_security_group_ids = [aws_security_group.my_sg.id]
 }
