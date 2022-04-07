4d9d57a56a4c6e8097f9cc4fe6822b83439945a0
iscaca <ismailcan9534@gmail.com>
2022-04-06 20:27:37 -0500

    count meta argument added

--- session-5/ec2.tf
+++ session-5/ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "my_ec2" {
+  count = 2                                                       # Meta argument 
   ami           = data.aws_ami.amazon_linux_2.image_id                           
   instance_type = var.instance_type                               #  "" String, Hard Coded Value
   tags = {
