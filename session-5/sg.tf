528fb77003596375d13f51b4cf5b798114e5fcfe
iscaca <ismailcan9534@gmail.com>
2022-04-06 21:14:23 -0500

    element functions are added

--- session-5/sg.tf
+++ session-5/sg.tf
@@ -6,12 +6,13 @@ resource "aws_security_group" "my_sg" {
 }
 
 resource "aws_security_group_rule" "ingress_ssh" {
+  count = length(var.ec2_inbound_rules)                                               // length will count how many string, number that I have in list
   type              = "ingress"
-  from_port         = 22
-  to_port           = 22
+  from_port         = element(var.ec2_inbound_rules, count.index)                     // count.index = 0, 1, 2
+  to_port           = element(var.ec2_inbound_rules, count.index)
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]               # [] = List
-  security_group_id = aws_security_group.my_sg.id # first_label.second_label.attribute
+  cidr_blocks       = [element(var.ec2_inbound_rules_cidr, count.index)]              # [] = List
+  security_group_id = aws_security_group.my_sg.id                                     # first_label.second_label.attribute
 
 }
 
@@ -30,3 +31,14 @@ resource "aws_security_group_rule" "egress" {
 # terraform.tfstate is called backend file.
 # terraform.tfstate or backend file keeps track of your infrastructure.
 # terraform.tfstate is in charge of keeping Terraform idempotent.
+
+
+# element, count.index, count, length = v.0.14 and v.0.15
+# Terraform introduced for_each
+# Terraform introduced dynamic resource creation
+
+# Interpolation
+# Iteration
+
+# For loop and while loop 
+# What is loop
--- session-5/variables.tf
+++ session-5/variables.tf
@@ -16,5 +16,17 @@ variable "instance_type" {
     default = "t2.micro"
 }
 
+variable "ec2_inbound_rules"{
+    type = list(string)
+    description = "A list of ports for inbound rule"
+    default = ["22", "80", "443"]
+}
+
+variable "ec2_inbound_rules_cidr"{
+    type = list(string)
+    description = "A list of CIDR ranges for inbound rules"
+    default = ["0.0.0.0/0", "10.0.0.0/16", "10.0.0.0/24"]
+}
+
 # Variables are for Re-Usabel purpose, something that is repeated over and over again.
 
