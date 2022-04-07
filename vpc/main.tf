2578a196ac24e046a7293a61ccebac9dd090e87f
iscaca <ismailcan9534@gmail.com>
2022-04-07 18:14:45 -0500

    main.tf is modified

--- vpc/main.tf
+++ vpc/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "main_ec2"{
         Environment = var.env
         Project = var.project
     }
-    subnet_id = aws_subnet.public_subnet_1a.id
+    subnet_id = aws_subnet.private_subnet_1a.id
     associate_public_ip_address = true
     vpc_security_group_ids = [aws_security_group.my_sg1.id]
     key_name = aws_key_pair.terraform_server_key.key_name 
