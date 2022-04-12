27de8c57dd586e08e7f2991cf20e71a697c98b20
iscaca <ismailcan9534@gmail.com>
2022-04-12 01:03:36 -0500

    Folder Structure is added

--- session-6/2(Folder Structure)/qa/backend.tf
+++ session-6/2(Folder Structure)/qa/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "terraform-march-2022-backend-bucket-donotdelete"
-        key = "session-6/_env_/terraform.tfstate"
+        key = "session-6/qa/terraform.tfstate"
         region = "us-east-1"
         dynamodb_table = "terraform-march-2022-lock"
     }
