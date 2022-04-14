423737e88e7cb7e108c649783e5e919735632d43
iscaca <ismailcan9534@gmail.com>
2022-04-13 20:32:13 -0500

    ec2.tf is modified and index.html added

--- session-8/ec2.tf
+++ session-8/ec2.tf
@@ -3,9 +3,27 @@ resource "aws_instance" "my_ec2" {
   ami           = data.aws_ami.amazon_linux_2.image_id                           
   instance_type = var.instance_type                             
   vpc_security_group_ids = [aws_security_group.my_sg.id]
-  user_data = file("user_data.sh")
+  key_name = aws_key_pair.terraform_server.key_name
+
+  provisioner "file" {
+    source = "/home/ec2-user/terraform-march-2022/session-8/index.html"
+    destination = "/tmp/index.html"
+
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      host = self.public_ip
+      private_key = file("~/.ssh/id_rsa")                   # Private key of my terraform server
+    }
+  
+  }
 
   tags = merge( local.common_tags, {
     Name = replace(local.name, "rtype","ec2")  } 
     )
+}
+
+resource "aws_key_pair" "terraform_server"{
+    key_name = replace(local.name, "rtype", "terraform_server_key")
+    public_key = file("~/.ssh/id_rsa.pub")
 }
--- session-8/user_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo yum install httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo echo “HELLO WORLD” >> /var/www/html/index.html
