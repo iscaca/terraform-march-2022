ab5a73d7ccaac510cf3ef9b1cab555abfd7670af
iscaca <ismailcan9534@gmail.com>
2022-04-14 03:57:24 -0500

    alb.tf is modified

--- session-7/frontend/alb.tf
+++ session-7/frontend/alb.tf
@@ -4,10 +4,12 @@ resource "aws_lb_target_group" "lb_tg"{
     port               = 80
     protocol           = "HTTP"
     target_type        = "instance"
+    
 
     }
 
 resource "aws_lb_listener" "http_listener" {
+  vpc_id            = "vpc-0f3d383f1ed7b8f53"
   load_balancer_arn = aws_lb.app_lb.arn
   port              = "80" 
   protocol          = "HTTP"
