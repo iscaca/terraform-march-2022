2f0c67f732de756f4bfbc4e79b17bbd7b7e6cf85
iscaca <ismailcan9534@gmail.com>
2022-04-14 03:11:16 -0500

    asg.tf is fixed

--- session-7/frontend/asg.tf
+++ session-7/frontend/asg.tf
@@ -1,18 +1,18 @@
 resource "aws_autoscaling_group" "frontend_asg" {
   name                 = "test-asg"
   min_size             = 1
-  max_size             = 3
-  desired_capacity     = 3
+  max_size             = 2
+  desired_capacity     = 2
   launch_configuration = aws_launch_configuration.as_conf.name
   vpc_zone_identifier  = var.subnet_id
   
-  
-  tags = merge (
-    local.common_tags, { propagate_at_lunch = true,
-      Name = replace(local.name, "rtype", "ASG") 
-      } 
+  tag {
+    key                 = "Name"
+    value               = merge(local.common_tags, { Name = replace(local.name, "rtype", "ASG") } )
+    propagate_at_launch = true
+  } 
       
-      )
+      
   
       
   
