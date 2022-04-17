5098535fe292d4461c5ef24acead1aa5209c9064
iscaca <ismailcan9534@gmail.com>
2022-04-17 11:17:56 -0500

    session-9 and child modules are added

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+    type = string
+    description = "This is AMI for my EC2 instance"
+}
+
+variable "instance_type" {
+    type = string
+    description = "This is a size of my EC2 instance"
+}
+
+variable "env" {
+    type = string
+    description = "This represents the environment"
+}
---
+++ session-9/dev/main.tf
@@ -0,0 +1,9 @@
+# Terraform Local module which consists of Child and Root modules
+
+module "ec2" {
+    source = "../../modules/ec2"                 #Local or Remote, when it is local module, you provide a path of the Child module
+    # You can declare your variables as a configuration
+    ami = "ami-0c02fb55956c7d316"
+    instance_type = "t2.micro"
+    env = "dev"
+}
